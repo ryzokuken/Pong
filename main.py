d090d32c13b18ee9d39334948e2eb552e51de95a
Ujjwal Sharma <usharma1998@gmail.com>
2017-11-05 20:44:28 +0530

    Solve linting issues

--- main.py
+++ main.py
@@ -18,19 +18,22 @@ speed_player = 10
 score_player = 0
 score_computer = 0
 
-def player_score():
-  global score_player
-  global pscore
 
-  score_player += 1
-  pscore.update_text(score_player)
+def player_score():
+    global score_player
+    global pscore
+
+    score_player += 1
+    pscore.update_text(score_player)
+
 
 def computer_score():
-  global score_computer
-  global cscore
+    global score_computer
+    global cscore
 
-  score_computer += 1
-  cscore.update_text(score_computer)
+    score_computer += 1
+    cscore.update_text(score_computer)
+
 
 paddles = pygame.sprite.Group()
 balls = pygame.sprite.Group()
@@ -38,7 +41,9 @@ displays = pygame.sprite.Group()
 
 player = paddle.Paddle(colors.BLUE, 20, 20)
 computer = paddle.Paddle(colors.RED, 760, 480)
-ball = ball.Ball(colors.GREEN, 390, 290, speed_ball, player_score, computer_score)
+ball = ball.Ball(colors.GREEN, 390, 290,
+                 speed_ball, player_score,
+                 computer_score)
 pscore = text.Text(default_font, '0', 200)
 cscore = text.Text(default_font, '0', 600)
 
@@ -47,29 +52,28 @@ balls.add(ball)
 displays.add([pscore, cscore])
 
 while not done:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      done = True
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
 
-  k = pygame.key.get_pressed()
-  if k[pygame.K_UP]:
-    player.move(-speed_player)
-  if k[pygame.K_DOWN]:
-    player.move(speed_player)
+    k = pygame.key.get_pressed()
+    if k[pygame.K_UP]:
+        player.move(-speed_player)
+    if k[pygame.K_DOWN]:
+        player.move(speed_player)
 
-  balls.update()
-  computer.rect.y = ball.rect.y - 40
-  for paddle in pygame.sprite.spritecollide(ball, paddles, False):
-    ball.speed_x = -ball.speed_x
-    ball.increase_speed(1)
-    print(ball.speed_x)
-    pygame.mixer.music.play()
+    balls.update()
+    computer.rect.y = ball.rect.y - 40
+    for p in pygame.sprite.spritecollide(ball, paddles, False):
+        ball.speed_x = -ball.speed_x
+        ball.increase_speed(1)
+        pygame.mixer.music.play()
 
-  screen.fill(colors.BLACK)
-  displays.draw(screen)
-  pygame.draw.line(screen, colors.WHITE, [400, 0], [400, 600], 3)
-  paddles.draw(screen)
-  balls.draw(screen)
-  pygame.display.flip()
+    screen.fill(colors.BLACK)
+    displays.draw(screen)
+    pygame.draw.line(screen, colors.WHITE, [400, 0], [400, 600], 3)
+    paddles.draw(screen)
+    balls.draw(screen)
+    pygame.display.flip()
 
-  clock.tick(60)
+    clock.tick(60)
